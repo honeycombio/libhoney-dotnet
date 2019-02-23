c2cbc8e77d3247feb3aa56df829a191e39df172b
Martin Thwaites <bitbucket@my2cents.co.uk>
2019-02-23 10:23:12 +0000

    Added Documentation to the Api Settings

--- src/Honeycomb/Models/HoneycombApiSettings.cs
+++ src/Honeycomb/Models/HoneycombApiSettings.cs
@@ -2,9 +2,28 @@ namespace Honeycomb.Models
 {
     public class HoneycombApiSettings
     {
+        /// <summary>
+        /// The TeamId within Honeycomb.
+        /// </summary>
+        /// <value></value>
         public string TeamId { get; set; }
+
+        /// <summary>
+        /// The default dataset to apply to each event.
+        /// </summary>
+        /// <value></value>
         public string DefaultDataSet { get; set; }
-        public int BatchSize { get; set; }
-        public int SendFrequency { get; set; }
+
+        /// <summary>
+        /// The default size of each push to the API.
+        /// </summary>
+        /// <value></value>
+        public int BatchSize { get; set; } = 100;
+
+        /// <summary>
+        /// The time between sending event batches.
+        /// </summary>
+        /// <value></value>
+        public int SendFrequency { get; set; } = 60;
     }
 }
