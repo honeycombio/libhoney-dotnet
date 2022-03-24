9b94ca10ed9a718c5b81608c17dc0041f576d8c5
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-03-24 13:06:47 +0000

    tidy up tests

--- test/Honeycomb.Tests/HoneycombApiSettingsTests.cs
+++ test/Honeycomb.Tests/HoneycombApiSettingsTests.cs
@@ -6,22 +6,21 @@ namespace Honeycomb.Models
 {
     public class HoneycombApiSettingsTests
     {
-        private const string ClassicWriteKey = "c1a551c000d68f9ed1e96432ac1a3380";
-        private const string ModernWriteKey = "d68f9ed1e96432ac1a3380";
-
         [Theory]
+        // defaults to empty string and does not trim dataset if set
         [InlineData(null, "", "")]
         [InlineData(null, "my-service", "my-service")]
         [InlineData(null, " my-service ", " my-service ")]
         [InlineData("", "", "")]
         [InlineData("", "my-service", "my-service")]
         [InlineData("", " my-service ", " my-service ")]
-        [InlineData(ClassicWriteKey, "", "")]
-        [InlineData(ClassicWriteKey, "my-service", "my-service")]
-        [InlineData(ClassicWriteKey, " my-service ", " my-service ")]
-        [InlineData(ModernWriteKey, "", "unknown_service")]
-        [InlineData(ModernWriteKey, "my-service", "my-service")]
-        [InlineData(ModernWriteKey, " my-service ", "my-service")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", "", "")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", "my-service", "my-service")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", " my-service ", " my-service ")]
+        // defaults to unknown_service and trims dataset if set
+        [InlineData("d68f9ed1e96432ac1a3380", "", "unknown_service")]
+        [InlineData("d68f9ed1e96432ac1a3380", "my-service", "my-service")]
+        [InlineData("d68f9ed1e96432ac1a3380", " my-service ", "my-service")]
         public void Dataset(string writekey, string dataset, string expected)
         {
             var settings = new HoneycombApiSettings
@@ -31,47 +30,5 @@ namespace Honeycomb.Models
             };
             settings.DefaultDataSet.ShouldBe(expected);
         }
-
-        // [Theory]
-        // [InlineData("", "")]
-        // [InlineData("my-service", "my-service")]
-        // [InlineData(" my-service ", " my-service ")]
-        // public void NoWriteKey(string dataset, string expected)
-        // {
-        //     var settings = new HoneycombApiSettings
-        //     {
-        //         WriteKey = "",
-        //         DefaultDataSet = dataset
-        //     };
-        //     settings.DefaultDataSet.ShouldBe(expected);
-        // }
-
-        // [Theory]
-        // [InlineData("", "")]
-        // [InlineData("my-service", "my-service")]
-        // [InlineData(" my-service ", " my-service ")]
-        // public void ClassicWriteKey(string dataset, string expected)
-        // {
-        //     var settings = new HoneycombApiSettings
-        //     {
-        //         WriteKey = "c1a551c000d68f9ed1e96432ac1a3380",
-        //         DefaultDataSet = dataset
-        //     };
-        //     settings.DefaultDataSet.ShouldBe(expected);
-        // }
-
-        // [Theory]
-        // [InlineData("", "unknown_service")]
-        // [InlineData("my-service", "my-service")]
-        // [InlineData("my-service ", "my-service")]
-        // public void ModernWriteKey(string dataset, string expected)
-        // {
-        //     var settings = new HoneycombApiSettings
-        //     {
-        //         WriteKey = "d68f9ed1e96432ac1a3380",
-        //         DefaultDataSet = dataset
-        //     };
-        //     settings.DefaultDataSet.ShouldBe(expected);
-        // }
-    }
+   }
 }
