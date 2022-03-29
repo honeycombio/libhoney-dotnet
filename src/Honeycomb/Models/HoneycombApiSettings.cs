71cfb05ab618e22a28a3efe545e3a68299053ecb
Mike Goldsmith <goldsmith.mike@gmail.com>
2022-03-29 14:46:36 +0100

    Add Environment and Services support (#60)

--- src/Honeycomb/Models/HoneycombApiSettings.cs
+++ src/Honeycomb/Models/HoneycombApiSettings.cs
@@ -1,9 +1,24 @@
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace Honeycomb.Models
 {
     public class HoneycombApiSettings
     {
+        private const string DefaultDatasetValue = "unknown_dataset";
+
+        private readonly ILogger<HoneycombApiSettings> _logger;
+        private string _dataset;
+
+        public HoneycombApiSettings()
+            : this(LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<HoneycombApiSettings>())
+        { }
+
+        public HoneycombApiSettings(ILogger<HoneycombApiSettings> logger)
+        {
+            _logger = logger;
+        }
+
         /// <summary>
         /// The TeamId within Honeycomb.
         /// </summary>
@@ -25,7 +40,27 @@ namespace Honeycomb.Models
         /// The default dataset to apply to each event.
         /// </summary>
         /// <value></value>
-        public string DefaultDataSet { get; set; }
+        public string DefaultDataSet
+        {
+            get
+            {
+                if (IsClassic) {
+                    return _dataset;
+                }
+                if (string.IsNullOrWhiteSpace(_dataset))
+                {
+                    _logger.LogWarning($"WARN: Dataset is empty or only whitespace, using default: '{DefaultDatasetValue}'");
+                    return DefaultDatasetValue;
+                }
+                var trimmed = _dataset.Trim();
+                if (_dataset != trimmed)
+                {
+                    _logger.LogWarning($"WARN: Dataset has unexpected whitespace, using trimmed version: '{trimmed}'");
+                }
+                return trimmed;
+            }
+            set { _dataset = value; }
+        }
 
         /// <summary>
         /// The default size of each push to the API.
@@ -43,5 +78,7 @@ namespace Honeycomb.Models
         /// The base URL to send event data to
         /// </summary>
         public string ApiHost { get; set; } = "https://api.honeycomb.io";
+
+        private bool IsClassic => string.IsNullOrWhiteSpace(WriteKey) || WriteKey.Length == 32;
     }
 }
---
+++ test/Honeycomb.Tests/HoneycombApiSettingsTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using Shouldly;
+
+namespace Honeycomb.Models
+{
+    public class HoneycombApiSettingsTests
+    {
+        [Theory]
+        // defaults to empty string and does not trim dataset if set
+        [InlineData(null, "", "")]
+        [InlineData(null, "my-service", "my-service")]
+        [InlineData(null, " my-service ", " my-service ")]
+        [InlineData("", "", "")]
+        [InlineData("", "my-service", "my-service")]
+        [InlineData("", " my-service ", " my-service ")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", "", "")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", "my-service", "my-service")]
+        [InlineData("c1a551c000d68f9ed1e96432ac1a3380", " my-service ", " my-service ")]
+        // defaults to unknown_dataset and trims dataset if set
+        [InlineData("d68f9ed1e96432ac1a3380", "", "unknown_dataset")]
+        [InlineData("d68f9ed1e96432ac1a3380", "my-service", "my-service")]
+        [InlineData("d68f9ed1e96432ac1a3380", " my-service ", "my-service")]
+        public void Dataset(string writekey, string dataset, string expected)
+        {
+            var settings = new HoneycombApiSettings
+            {
+                WriteKey = writekey,
+                DefaultDataSet = dataset
+            };
+            settings.DefaultDataSet.ShouldBe(expected);
+        }
+   }
+}
